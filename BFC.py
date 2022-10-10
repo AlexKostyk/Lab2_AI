9c25b9459b1d8cafd079498aed98852b8229f1fa
aleks <alekskostyk2012@gmail.com>
2022-10-10 19:08:24 +0300

    1

---
+++ BFC.py
@@ -0,0 +1,3 @@
+from collections import deque
+
+def bfs_method(start, end, graph):
--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import BFC
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+city1 = np.loadtxt('data.txt', dtype = 'str', usecols=(0), encoding='utf-8')
+city2 = np.loadtxt('data.txt', dtype = 'str', usecols=(1), encoding='utf-8')
+distance = np.loadtxt('data.txt', dtype = 'i2', usecols=(2), encoding='utf-8')
 
+for item in city1:
+graph =
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
